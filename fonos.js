0816cc128a3f89047fd70b90f28796085e92ca9e
hipacket <48793270+hipacket@users.noreply.github.com>
2021-08-19 09:57:25 +0700

    Update fonos.js

--- fonos.js
+++ fonos.js
@@ -1,19 +1,12 @@
 var obj = JSON.parse($response.body);
 
 if(obj.length) {
-  var book = obj.find(function(o) {
+  var book_index = obj.findIndex(function(o) {
     return o.value.id;
   });
-  var data = obj.find(function(o) {
-    return o.value.data && o.value.data.length;
-  });;
-  if(book) {
-    book.isFree = true;
-    book.whyItFree = 'Fonos quyết định dành tặng các thính giả hoàn toàn miễn phí.';
-  } else if(data) {
-    data.forEach(function(b) {
-      b.isFree = true;
-    });
+  if(book_index) {
+    obj[book_index].value.isFree = true;
+    obj[book_index].value.whyItFree = 'Fonos quyết định dành tặng các thính giả hoàn toàn miễn phí.';
   }
 }
 
