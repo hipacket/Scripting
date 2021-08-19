9e9e1085f6c2d345ef89869aa1602702eb2e6044
hipacket <48793270+hipacket@users.noreply.github.com>
2021-08-19 10:17:47 +0700

    Update fonos.js

--- fonos.js
+++ fonos.js
@@ -7,6 +7,7 @@ if(obj.length) {
   if(book_index) {
     obj[book_index].value.isFree = true;
     obj[book_index].value.whyItFree = 'Fonos quyết định dành tặng các thính giả hoàn toàn miễn phí.';
+    obj[book_index].value.purchased = true;
   }
 }
 
