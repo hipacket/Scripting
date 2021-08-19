8e3eb8b06d0006b70bdcb9da0ad25d3d9a9705f7
hipacket <48793270+hipacket@users.noreply.github.com>
2021-08-19 08:58:22 +0700

    Create fonos.js

---
+++ fonos.js
@@ -0,0 +1,13 @@
+var obj = JSON.parse($response.body);
+
+if(obj.length && obj[0].value) {
+  if(obj[0].value.id) {
+    obj[0].value.isFree = true;
+  } else if(obj[0].value.data && obj[0].value.data.length) {
+    obj[0].value.data.forEach(book => {
+      book.isFree = true;
+    });
+  }
+}
+
+$done({status: 200, body: JSON.stringify(obj)});
