8a4414c42900709abf79f013bde7348541558bc8
hipacket <48793270+hipacket@users.noreply.github.com>
2021-10-21 11:47:55 +0700

    Update funEnglish.js

--- funEnglish.js
+++ funEnglish.js
@@ -1,6 +1,7 @@
 var obj = JSON.parse($response.body);
 var plan = obj.find(p => p.has_expiration);
 if(plan) {
+  plan.status = 'wcm-active';
   plan.has_expiration = false;
   plan.expires_gmt = 4071200107;
   plan.expires_gmt_formatted = '2099-01-04 15:55:07';
