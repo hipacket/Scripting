39790d7e27d19ea2a7e6c04804d6024115c1012e
hipacket <48793270+hipacket@users.noreply.github.com>
2022-03-02 20:56:16 +0700

    Update vKids.js

--- vKids.js
+++ vKids.js
@@ -1,4 +1,8 @@
 var obj = JSON.parse($response.body);
-obj.data.premium = 1;
+if(obj.data.userData) {
+  obj.data.userData.premium = 1;
+} else {
+  obj.data.premium = 1;
+}
 
 $done({body: JSON.stringify(obj)});
