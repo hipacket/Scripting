2cf831896f2498af704260f2fcfa299a8510d11c
hipacket <48793270+hipacket@users.noreply.github.com>
2022-03-05 11:31:13 +0700

    Update lingokids.vip.js

--- lingokids.vip.js
+++ lingokids.vip.js
@@ -1,17 +1,33 @@
 var obj = JSON.parse($response.body);
 
-obj["info"]["subscriptions"]= [{
-	"status": "active",
-	"product": "unlimited",
-	"duration_unit": "months",
-	"id": 805063,
-	"platform": "apple",
-	"duration_value": 1,
-	"starts_at": 1572617692,
-	"ends_at": 4099821292,
-	"auto_renew_status": true,
-	"plan_id": "lk.ios.s1m.t1m.p15.v1",
-	"state": "active"
+// obj["info"]["subscriptions"]= [{
+// 	"status": "active",
+// 	"product": "unlimited",
+// 	"duration_unit": "months",
+// 	"id": 805063,
+// 	"platform": "apple",
+// 	"duration_value": 1,
+// 	"starts_at": 1572617692,
+// 	"ends_at": 4099821292,
+// 	"auto_renew_status": true,
+// 	"plan_id": "lk.ios.s1m.t1m.p15.v1",
+// 	"state": "active"
+// }];3369406
+
+
+obj.info.cohort = "37";
+obj.info.subscriptions = [{
+  "id": 805063,
+  "status": "active",
+  "in_grace_period": false,
+  "product": "unlimited",
+  "platform": "apple",
+  "starts_at": 1646452089,
+  "ends_at": 4099821292,
+  "plan_id": "s1m.t7d.p8.v1",
+  "duration_unit": "months",
+  "duration_value": 1,
+  "auto_renew_status": true
 }];
 
 $done({body: JSON.stringify(obj)});
