73afed1b7260df0026f74fe8ac5be0eddf1adaf7
hipacket <48793270+hipacket@users.noreply.github.com>
2022-03-10 16:07:09 +0700

    Update umbalena.js

--- umbalena.js
+++ umbalena.js
@@ -1,14 +1,8 @@
 var obj = JSON.parse($response.body);
-if(obj.personal) {
-  obj.personal.personalpackage_set = [{
-    "expiry_date": "01/10/2099"
-  }];
-  obj.personal.expiry_date = "10/01/2099 09:27";
-} else {
-  obj.personalpackage_set = [{
-    "expiry_date": "01/10/2099"
-  }];
-  obj.expiry_date = "10/01/2099 09:27";
+if(obj.results) {
+  for(const book of obj.results) {
+    book.is_free = true;
+  }
 }
 $done({body: JSON.stringify(obj)});
 
