0077ce590006f306995a5ae5e4bc0290b80a90fa
hipacket <48793270+hipacket@users.noreply.github.com>
2022-03-10 16:16:26 +0700

    Update umbalena.js

--- umbalena.js
+++ umbalena.js
@@ -1,8 +1,8 @@
 var obj = JSON.parse($response.body);
 if(obj.results) {
-  for(const book of obj.results) {
-    book.is_free = true;
-  }
+  obj.results.forEach((book, i) => {
+    obj.results[i].is_free = true;
+  });
 }
 $done({body: JSON.stringify(obj)});
 
