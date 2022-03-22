434cd81cf93034c14c894cd77a81043b7c89973d
hipacket <48793270+hipacket@users.noreply.github.com>
2022-03-22 15:18:39 +0700

    Update funEnglish.js

--- funEnglish.js
+++ funEnglish.js
@@ -7,4 +7,12 @@ if(plan) {
   plan.expires_gmt_formatted = '2099-01-04 15:55:07';
 }
 
-$done({body: JSON.stringify(obj)});
+
+$done({body: JSON.stringify([{
+	"user_id": "59b34681-50da-4d1b-9e74-4183b2dcf319",
+	"plan": "fe",
+	"status": "active",
+	"has_expiration": false,
+	"expires_ms": 4071200107,
+	"expires_formatted": "2099-01-04T08:55:07.000Z"
+}])});
