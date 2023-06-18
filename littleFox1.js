22e5cf957cd74afe15ca52cf87e8dd502648e02a
hipacket <48793270+hipacket@users.noreply.github.com>
2023-06-18 09:58:39 +0700

    Update littleFox1.js

--- littleFox1.js
+++ littleFox1.js
@@ -1,18 +1,26 @@
 var obj = JSON.parse($response.body);
 if(obj.data.contents.length) {
 	for(let i = 0; i < obj.data.contents.length; i++) {
-		obj.data.contents[i].service_info = {
-			"service": "F",
+    obj.data.contents[i].service_info = {
+      "service": "F",
 			"story": "F",
 			"original_text": "F",
-			"vocabulary": "N",
-			"quiz": "F",
-			"sentence": "N",
-			"crossword": "N",
-			"translate": "N",
-			"starwords": "N",
-			"ebook": "F"
-		};
+      "sentence": "F",
+      "ebook": "F",
+      ...obj.data.contents[i].service_info
+    };
+		// obj.data.contents[i].service_info = {
+		// 	"service": "F",
+		// 	"story": "F",
+		// 	"original_text": "F",
+		// 	"vocabulary": "N",
+		// 	"quiz": "F",
+		// 	"sentence": "N",
+		// 	"crossword": "N",
+		// 	"translate": "N",
+		// 	"starwords": "N",
+		// 	"ebook": "F"
+		// };
 	}
 }
 $done({body: JSON.stringify(obj)});
