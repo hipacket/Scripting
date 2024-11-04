40285cefa86c1cfda02b05d07f32d42a13f8f466
hipacket <48793270+hipacket@users.noreply.github.com>
2024-11-04 11:14:19 +0700

    Update monkey.js

--- monkey.js
+++ monkey.js
@@ -24,7 +24,7 @@ if(obj.data.purchased && obj.data.purchased.is_purchased) {
 	obj.data.purchased.time_expire = 0;
 	obj.data.purchased.free_days = 365;
 	obj.data.purchased.is_free = false;
-	obj.data.userInfo.time_expired = 4070908800;
+	obj.data.userInfo.time_expired = Math.round(new Date()/1000) + 86400 * 365;
 	obj.data.userInfo.is_trial = 0;
 } else {
 	for(var key of Object.keys(obj.data.purchased)) {
@@ -32,7 +32,8 @@ if(obj.data.purchased && obj.data.purchased.is_purchased) {
 		obj.data.purchased[key].is_active = true;
 		obj.data.purchased[key].time_expire = 0;
 		obj.data.purchased[key].free_days = 365;
-		obj.data.purchased[key].is_free = false;
+		obj.data.purchased[key].free_user = false;
+		obj.data.time_expire = Math.round(new Date()/1000) + 86400 * 365;
 	}
 }
 $done({body: JSON.stringify(obj)});
