48b21a89edca1a5c8dd70405cfdb1af25ef2da7e
hipacket <48793270+hipacket@users.noreply.github.com>
2025-07-30 10:07:38 +0700

    Update lingokids_request.js

--- lingokids_request.js
+++ lingokids_request.js
@@ -2,4 +2,10 @@ var headers = $request.headers
 if(headers['If-None-Match']) {
   delete headers['If-None-Match'];
 }
+if(headers['if-none-match']) {
+  delete headers['if-none-match'];
+}
+if(headers['if-none-match']) {
+  delete headers['if-none-match'];
+}
 $done({headers});
